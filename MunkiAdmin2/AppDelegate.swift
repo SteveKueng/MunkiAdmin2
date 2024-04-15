f02125f84c32f5e0711be61c7d428856a2c9a73f
Steve Kueng <steve.kueng@umb.ch>
2024-04-15 20:18:21 +0200

    Initial Commit

---
+++ MunkiAdmin2/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MunkiAdmin2
+//
+//  Created by Steve Küng on 15.04.2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MunkiAdmin2UITests/MunkiAdmin2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MunkiAdmin2UITestsLaunchTests.swift
+//  MunkiAdmin2UITests
+//
+//  Created by Steve Küng on 15.04.2024.
+//
+
+import XCTest
+
+final class MunkiAdmin2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
